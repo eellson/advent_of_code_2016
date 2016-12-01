04d5ee12628aaf4af81a715d7be0c60b52fcbbdf
Ed Ellson <eellson@gmail.com>
2016-12-01 22:33:33 +0000

    Deconstruct instructions with pattern match
    Testing this out as initial method for splitting out a rotation from a
    translation, which I want for part b.

--- lib/advent_of_code_2016/day_1.ex
+++ lib/advent_of_code_2016/day_1.ex
@@ -8,12 +8,29 @@ defmodule AdventOfCode.Day1 do
     abs(x) + abs(y)
   end
 
-  def do_move("R" <> n, {:n, {x, y}}), do: {:e, {x + String.to_integer(n), y}}
-  def do_move("L" <> n, {:n, {x, y}}), do: {:w, {x - String.to_integer(n), y}}
-  def do_move("R" <> n, {:e, {x, y}}), do: {:s, {x, y - String.to_integer(n)}}
-  def do_move("L" <> n, {:e, {x, y}}), do: {:n, {x, y + String.to_integer(n)}}
-  def do_move("R" <> n, {:s, {x, y}}), do: {:w, {x - String.to_integer(n), y}}
-  def do_move("L" <> n, {:s, {x, y}}), do: {:e, {x + String.to_integer(n), y}}
-  def do_move("R" <> n, {:w, {x, y}}), do: {:n, {x, y + String.to_integer(n)}}
-  def do_move("L" <> n, {:w, {x, y}}), do: {:s, {x, y - String.to_integer(n)}}
+  def do_move("R" <> distance, {:n, coords}), do: do_move(distance, {:e, coords})
+  def do_move("R" <> distance, {:e, coords}), do: do_move(distance, {:s, coords})
+  def do_move("R" <> distance, {:s, coords}), do: do_move(distance, {:w, coords})
+  def do_move("R" <> distance, {:w, coords}), do: do_move(distance, {:n, coords})
+
+  def do_move("L" <> distance, {:n, coords}), do: do_move(distance, {:w, coords})
+  def do_move("L" <> distance, {:e, coords}), do: do_move(distance, {:n, coords})
+  def do_move("L" <> distance, {:s, coords}), do: do_move(distance, {:e, coords})
+  def do_move("L" <> distance, {:w, coords}), do: do_move(distance, {:s, coords})
+
+  def do_move(string_distance, current) when is_binary string_distance do
+    string_distance
+    |> String.to_integer
+    |> do_move(current)
+  end
+
+  def do_move(1, {:n, {x, y}}), do: {:n, {x, y + 1}}
+  def do_move(1, {:e, {x, y}}), do: {:e, {x + 1, y}}
+  def do_move(1, {:s, {x, y}}), do: {:s, {x, y - 1}}
+  def do_move(1, {:w, {x, y}}), do: {:w, {x - 1, y}}
+
+  def do_move(n, current) do
+    next = do_move(1, current)
+    do_move(n - 1, next)
+  end
 end
