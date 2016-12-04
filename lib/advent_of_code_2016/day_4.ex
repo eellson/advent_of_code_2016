ee919dcb79a29e9f3061a521b8c4e9d40d3d6605
Ed Ellson <eellson@gmail.com>
2016-12-04 13:47:07 +0000

    Solve 4.b

--- lib/advent_of_code_2016/day_4.ex
+++ lib/advent_of_code_2016/day_4.ex
@@ -9,6 +9,16 @@ defmodule AdventOfCode.Day4 do
     |> Enum.reduce(0, &+/2)
   end
 
+  def run_b do
+    "./lib/advent_of_code_2016/day_4/input.txt"
+    |> File.read!
+    |> String.split
+    |> Enum.map(&into_map/1)
+    |> Enum.filter(&is_real?/1)
+    |> Enum.map(&decipher_name/1)
+    |> Enum.filter(fn %{"name" => name} -> String.contains?(name, "north") end)
+  end
+
   def into_map(string) do
     Regex.named_captures(~r/^(?<name>.+)-(?<id>\d+)\[(?<check>\w+)\]/, string)
   end
@@ -29,4 +39,18 @@ defmodule AdventOfCode.Day4 do
   end
 
   def count_into(char, map), do: Map.update(map, char, 1, &(&1 + 1))
+
+  def decipher_name(%{"name" => name, "id" => id} = map) do
+    %{map|"name" => shift_name(name, String.to_integer(id))}
+  end
+
+  def shift_name(name, count) do
+    name
+    |> String.to_charlist
+    |> Enum.map(&rotate_char(&1, count))
+    |> List.to_string
+  end
+
+  def rotate_char(?-, _), do: ?\s
+  def rotate_char(char, count), do: rem(char + count - 97, 26) + 97
 end
