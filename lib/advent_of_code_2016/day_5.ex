df4752c9a11f55a5629e8e9d1d04df9ef2ca2c55
Ed Ellson <eellson@gmail.com>
2016-12-05 19:11:11 +0000

    Refactor various steps into named functions
    This seems clearer?

--- lib/advent_of_code_2016/day_5.ex
+++ lib/advent_of_code_2016/day_5.ex
@@ -4,12 +4,8 @@ defmodule AdventOfCode.Day5 do
 
     Stream.iterate(0, &(&1 + 1))
     |> Stream.map(&finalize_and_encode(hash_context, &1))
-    |> Stream.filter(fn hex -> test_with(hex, &handle_hex/1) end)
-    |> Enum.reduce_while([], fn
-      _hex, acc when length(acc) >= 8 -> {:halt, acc}
-      hex, acc -> {:cont, [hex|acc]} |> IO.inspect
-    end)
-    |> Enum.map(&get_character/1)
+    |> Stream.filter_map(&five_zeros/1, &get_character/1)
+    |> Enum.reduce_while([], &acc_incomplete/2)
     |> Enum.reverse
     |> List.to_string
   end
@@ -19,36 +15,39 @@ defmodule AdventOfCode.Day5 do
 
     Stream.iterate(0, &(&1 + 1))
     |> Stream.map(&finalize_and_encode(hash_context, &1))
-    |> Stream.filter(fn hex -> test_with(hex, &handle_hex/1) end)
-    |> Enum.reduce_while([], fn
-      _hex, acc when length(acc) >= 8 -> {:halt, acc}
-      hex, acc ->
-        acc = hex |> pos_and_char |> put_new_if_valid(acc)
-
-        {:cont, acc} |> IO.inspect
-    end)
+    |> Stream.filter_map(&zeros_and_position/1, &pos_and_char_tuple/1)
+    |> Enum.reduce_while([], &put_new/2)
     |> Enum.sort_by(fn {pos, char} -> pos |> Atom.to_string |> String.to_integer end)
     |> Enum.map(fn {pos, char} -> char end)
     |> List.to_string
   end
 
-  def test_with(hash_context, test_fn), do: hash_context |> test_fn.()
+  def five_zeros(<<"00000", rest::binary>>), do: true
+  def five_zeros(_hex), do: false
 
-  def handle_hex(<<"00000", rest::binary>>), do: true
-  def handle_hex(_hex), do: false
+  @valid_positions ["0", "1", "2", "3", "4", "5", "6", "7"]
+  def zeros_and_position(<<"00000",
+                     pos::binary-size(1),
+                     char::binary-size(1),
+                     _rest::binary>>) when pos in @valid_positions do
+    true
+  end
+  def zeros_and_position(_), do: false
 
-  def pos_and_char(<<"00000",
+  def pos_and_char_tuple(<<"00000",
                      pos::binary-size(1),
                      char::binary-size(1),
                      _rest::binary>>) do
     {pos, char}
   end
 
-  @valid_positions ["0", "1", "2", "3", "4", "5", "6", "7"]
-  def put_new_if_valid({pos, val}, keyword) when pos in @valid_positions do
-    Keyword.put_new(keyword, String.to_atom(pos), val)
+  def acc_incomplete(_char, acc) when length(acc) >= 8, do: {:halt, acc}
+  def acc_incomplete(char, acc), do: {:cont, [char|acc]} |> IO.inspect
+
+  def put_new(_, keyword) when length(keyword) >= 8, do: {:halt, keyword}
+  def put_new({pos, val}, keyword) do
+    {:cont, Keyword.put_new(keyword, String.to_atom(pos), val)} |> IO.inspect
   end
-  def put_new_if_valid(_, keyword), do: keyword
 
   def get_character(<<"00000", char::binary-size(1), rest::binary>>), do: char
 
