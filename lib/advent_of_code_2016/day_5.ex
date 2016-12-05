d72d1dcf5f86ad88d080959f4e446f95d40be155
Ed Ellson <eellson@gmail.com>
2016-12-05 14:00:33 +0000

    Pass acumulation function into reduce

--- lib/advent_of_code_2016/day_5.ex
+++ lib/advent_of_code_2016/day_5.ex
@@ -5,25 +5,25 @@ defmodule AdventOfCode.Day5 do
     Stream.iterate(0, &(&1 + 1))
     |> Enum.reduce_while([], fn
       _n, acc when length(acc) >= 8 -> {:halt, acc}
-      n, acc -> test(hash_context, acc, n)
+      n, acc -> test(hash_context, acc, n, &handle_hex/2)
     end)
   end
 
-  def test(hash_context, acc, i) do
+  def test(hash_context, acc, i, test_and_accumulate_fn) do
     acc =
       hash_context
       |> :crypto.hash_update(Integer.to_string(i))
       |> :crypto.hash_final
-      |> Base.encode16
-      |> handle_hex(acc)
+      |> Base.encode16(case: :lower)
+      |> test_and_accumulate_fn.(acc)
 
     {:cont, acc}
   end
 
-  def handle_hex(<<"00000", char::binary-size(1), rest::binary>>, acc) do
-    [char|acc]
-  end
+  def handle_hex(<<"00000", rest::binary>>, acc), do: [rest|acc] |> IO.inspect
   def handle_hex(_hex, acc), do: acc
+
+  def get_character(<<char::binary-size(1), rest::binary>>), do: char
 
   def new_hash_context(data) do
     :md5 |> :crypto.hash_init |> :crypto.hash_update(data)
