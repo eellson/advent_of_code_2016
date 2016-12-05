097cf95db21f4e65248366e65863d9b1b1915470
Ed Ellson <eellson@gmail.com>
2016-12-05 17:03:31 +0000

    Refactor into Streams
    Moves much of the logic into Stream instead of in the reducing function.
    Bit cleaner (although I think it is certainly a hack to use Keyword like
    I am. Doing so because I want to be able to `put_new` and also want to
    be able to call `length` on it in a guard...

--- lib/advent_of_code_2016/day_5.ex
+++ lib/advent_of_code_2016/day_5.ex
@@ -3,9 +3,11 @@ defmodule AdventOfCode.Day5 do
     hash_context = "ojvtpuvg" |> new_hash_context
 
     Stream.iterate(0, &(&1 + 1))
+    |> Stream.map(&finalize_and_encode(hash_context, &1))
+    |> Stream.filter(fn hex -> test_with(hex, &handle_hex/1) end)
     |> Enum.reduce_while([], fn
-      _n, acc when length(acc) >= 8 -> {:halt, acc}
-      n, acc -> test(hash_context, acc, n, &handle_hex/2)
+      _hex, acc when length(acc) >= 8 -> {:halt, acc}
+      hex, acc -> {:cont, [hex|acc]} |> IO.inspect
     end)
     |> Enum.map(&get_character/1)
     |> Enum.reverse
@@ -13,48 +15,51 @@ defmodule AdventOfCode.Day5 do
   end
 
   def run_b do
-    hash_context = "abc" |> new_hash_context
+    hash_context = "ojvtpuvg" |> new_hash_context
 
     Stream.iterate(0, &(&1 + 1))
+    |> Stream.map(&finalize_and_encode(hash_context, &1))
+    |> Stream.filter(fn hex -> test_with(hex, &handle_hex/1) end)
     |> Enum.reduce_while([], fn
-      _n, acc when length(acc) >= 8 -> {:halt, acc}
-      n, acc -> test(hash_context, acc, n, &interesting_and_valid_hex/2)
+      _hex, acc when length(acc) >= 8 -> {:halt, acc}
+      hex, acc ->
+        acc = hex |> pos_and_char |> put_new_if_valid(acc)
+
+        {:cont, acc} |> IO.inspect
     end)
-    |> Enum.sort_by(fn {pos, char} -> String.to_integer(pos) end)
+    |> Enum.sort_by(fn {pos, char} -> pos |> Atom.to_string |> String.to_integer end)
     |> Enum.map(fn {pos, char} -> char end)
     |> List.to_string
   end
 
-  def test(hash_context, acc, i, test_and_accumulate_fn) do
-    acc =
-      hash_context
-      |> :crypto.hash_update(Integer.to_string(i))
-      |> :crypto.hash_final
-      |> Base.encode16(case: :lower)
-      |> test_and_accumulate_fn.(acc)
+  def test_with(hash_context, test_fn), do: hash_context |> test_fn.()
 
-    {:cont, acc}
+  def handle_hex(<<"00000", rest::binary>>), do: true
+  def handle_hex(_hex), do: false
+
+  def pos_and_char(<<"00000",
+                     pos::binary-size(1),
+                     char::binary-size(1),
+                     _rest::binary>>) do
+    {pos, char}
   end
 
-  def handle_hex(<<"00000", rest::binary>>, acc), do: [rest|acc] |> IO.inspect
-  def handle_hex(_hex, acc), do: acc
-
-  def interesting_and_valid_hex(<<"00000", pos::binary-size(1),
-                                  char::binary-size(1), _rest::binary>>, acc)
-      when pos in ["0", "1", "2", "3", "4", "5", "6", "7"] do
-          case Enum.any?(acc, fn
-                           {^pos, _char} -> true
-                           _ -> false
-          end) do
-            false -> [{pos, char}|acc] |> IO.inspect
-            _ -> acc
-          end
+  @valid_positions ["0", "1", "2", "3", "4", "5", "6", "7"]
+  def put_new_if_valid({pos, val}, keyword) when pos in @valid_positions do
+    Keyword.put_new(keyword, String.to_atom(pos), val)
   end
-  def interesting_and_valid_hex(_hex, acc), do: acc
+  def put_new_if_valid(_, keyword), do: keyword
 
-  def get_character(<<char::binary-size(1), rest::binary>>), do: char
+  def get_character(<<"00000", char::binary-size(1), rest::binary>>), do: char
 
   def new_hash_context(data) do
     :md5 |> :crypto.hash_init |> :crypto.hash_update(data)
+  end
+
+  def finalize_and_encode(hash_context, i) do
+    hash_context
+    |> :crypto.hash_update(Integer.to_string(i))
+    |> :crypto.hash_final
+    |> Base.encode16(case: :lower)
   end
 end
