0fdb4877bb2a4ab934a49b0ac3bacffa4a7ae012
Ed Ellson <eellson@gmail.com>
2016-12-10 23:57:20 +0000

    Solve day 9 part b
    This is *not good*. Although i'm not attempting to store the
    decompressed string in memory all at once, I am actually decompressing
    the entire thing and so this approach is super slow.
    
    My excuse is that I need to catch up 😰

--- lib/advent_of_code_2016/day_9.ex
+++ lib/advent_of_code_2016/day_9.ex
@@ -3,20 +3,47 @@ defmodule AdventOfCode.Day9 do
     "./lib/advent_of_code_2016/day_9/input.txt"
     |> File.read!()
     |> decompress()
-    |> String.length
+    |> String.length()
   end
 
-  def decompress(string) do
-    string
-    |> scan_for_markers()
+  # this takes 20 mins to run pls don't judge me programming gods
+  def run_b do
+    "./lib/advent_of_code_2016/day_9/input.txt"
+    |> File.read!()
+    |> calc_decompress_size()
   end
+
+  def calc_decompress_size(string), do: string |> scan_for_markers_ii()
+
+  def scan_for_markers_ii(string), do: do_scan_for_markers_ii(string, 0)
+
+  def do_scan_for_markers_ii("", size), do: size
+  def do_scan_for_markers_ii("(" <> rest, size) do
+    count_decompress(rest, size)
+  end
+  def do_scan_for_markers_ii(<<char::binary-size(1), rest::binary>>, size) do
+    do_scan_for_markers_ii(rest, size + 1)
+  end
+
+  def count_decompress(string, size) do
+    %{"len" => len, "rep" => rep, "next" => next} =
+      ~r/(?<len>\d+)x(?<rep>\d+)\)(?<next>.*)/
+      |> Regex.named_captures(string)
+
+    {to_rep, remaining} = String.split_at(next, String.to_integer(len))
+
+    expansion = String.duplicate(to_rep, String.to_integer(rep))
+
+    size = do_scan_for_markers_ii(expansion, size)
+    do_scan_for_markers_ii(remaining, size)
+  end
+
+  def decompress(string), do: string |> scan_for_markers()
 
   def scan_for_markers(string), do: do_scan_for_markers(string, [])
 
   def do_scan_for_markers("", decompressed) do
-    decompressed
-    |> Enum.reverse
-    |> Enum.join
+    decompressed |> Enum.reverse |> Enum.join
   end
   def do_scan_for_markers("(" <> rest, decompressed) do
      marker_state(rest, decompressed)
@@ -32,6 +59,7 @@ defmodule AdventOfCode.Day9 do
 
     {to_rep, remaining} = String.split_at(next, String.to_integer(len))
 
-    do_scan_for_markers(remaining, [String.duplicate(to_rep, String.to_integer(rep))|decompressed])
+    decompressed = [String.duplicate(to_rep, String.to_integer(rep))|decompressed]
+    do_scan_for_markers(remaining, decompressed)
   end
 end
